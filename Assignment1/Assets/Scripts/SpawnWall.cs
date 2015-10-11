5d45334a8276cd7febaec07bc7b3706c088d9f0e
niccojacinto <nix_jacinto25@hotmail.com>
2015-10-11 19:14:17 -0400

    Assignment1
    Commit for GAME200-assignment1
    
    8x8 wall projectile
    
    -gitignore added, but uncertain if it would work smoothly

---
+++ Assignment1/Assets/Scripts/SpawnWall.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnWall : MonoBehaviour {
+
+
+    public Rigidbody wall;
+    public float xspacing;
+    public float yspacing;
+    public bool offset = true;
+    public int length;
+    public int height;
+
+    // Use this for initialization
+    void Start() {
+
+        Vector3 wallSize = wall.GetComponent<Renderer>().bounds.size;
+
+
+        if (offset) {
+            float offsetAmount;
+            for (int x = 0; x < length; x++) {
+                for (int y = 0; y < height; y++) {
+                    offsetAmount = (y % 2 == 0) ? 0 : wallSize.x / 2;
+                    Rigidbody instantiateWall = Instantiate(wall,
+                        transform.position + new Vector3(x * (wallSize.x + xspacing) + offsetAmount, y * (wallSize.y + yspacing)),
+                        transform.rotation) as Rigidbody;
+                }
+            }
+            return;
+        }
+       
+        for (int x = 0; x < length; x++) {
+            for (int y = 0; y < height; y++) {
+                Rigidbody instantiateWall = Instantiate(wall,
+                    transform.position + new Vector3(x * wallSize.x + (x * xspacing), y * wallSize.y + (y * yspacing)),
+                    transform.rotation) as Rigidbody;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
